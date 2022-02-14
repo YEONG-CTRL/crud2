dfecd1a17a6719bf2b98053b3d3a238b1c79f42d
Park YengSeo <1park4170@gmail.com>
2022-02-14 15:44:31 +0900

    completed post

--- owners/views.py
+++ owners/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
+
+#from django.shortcuts import render
+from django.http   import JsonResponse
+from django.views import View
+#from flask           import request
+
+from owners.models import Owners,Dogs
 
 # Create your views here.
+class OwnersView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        owner = Owners.objects.create(
+            name  = data['owner'],
+            email  = data['email'],
+            age    = data['age'],       
+            )
+        dog = Dogs.objects.create(
+            owner = owner,
+            name  = data['dog'],
+            age    = data['dog_age']    
+            )
+        return JsonResponse({'message':'created'}, status=201) # 201 created
