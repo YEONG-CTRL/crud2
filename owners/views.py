694eb964491197104195a96d7d110527be899313
Park YengSeo <1park4170@gmail.com>
2022-02-14 20:51:04 +0900

    finished GET

--- owners/views.py
+++ owners/views.py
@@ -25,6 +25,34 @@ class OwnersView(View):
         '''      
         return JsonResponse({'message':'created'}, status=201) # 201 created
 
+    def get(self, request):
+        owners = Owners.objects.all()
+        results = []
+
+        for owner in owners:
+            for owner_dog in owner.dogs_set.all():
+                results.append(
+                    {
+                        "name" : owner.name,
+                        "email" : owner.email,
+                        "age"   : owner.age,
+                        "owner's_dog" : owner_dog.name,
+                        "owner's_dog_age": owner_dog.age,
+                    }
+                )    
+        return JsonResponse({'results':results}, status=200)
+
+                        # "owner's_dog": [
+                        #     for i in owner.dogs_set.all():
+                        #         {"dog_name":print(i.name),
+                        #          "dog_age"  :print(i.age) 
+                        #         ]
+                        # }
+              
+        
+        
+
+
 class DogsView(View):
     def post(self,request):
         data = json.loads(request.body)
@@ -36,3 +64,17 @@ class DogsView(View):
             age    = data['dog_age']    
             )
         return JsonResponse({'message':'created'}, status=201) # 201 created
+
+    def get(self,request):
+        dogs =Dogs.objects.all()
+        results = []
+
+        for dog in dogs:
+            results.append(
+                {
+                    "name": dog.name,
+                    "age": dog.age,
+                    "owner":dog.owner.name,
+                }
+            )
+        return JsonResponse({'results':results}, status=200)
